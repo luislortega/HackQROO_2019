7dda161c9876871f40a8278645feac1f0bd5b083
luislortega <a18016323@alumnos.uady.mx>
2019-12-06 18:33:28 -0500

    Cantidad de alumnos almacenados y limpiados

--- BACKEND/seed/index.js
+++ BACKEND/seed/index.js
@@ -16,17 +16,20 @@ sequelize.sync({ force: true }).then(async function() {
       municipio.create(muni);
     }),
   ).then(function(one) {
-
     /* LIMPIEZA DE DATOS */
     let array_apoyo_trabajo_escuela = new Array();
+    let array_cantidad_estudiantes = new Array();
+
     let contador_apoyo_trabajo_escuela = 0;
+    let contador_cantidad_estudiantes = 0;
+
     let fs = require('fs');
     let parse = require('csv-parse');
     let async = require('async');
 
     //Archivos CSV
     let trabajoestudio = 'src/DATA/educacion/apoyotrabajoestudio.csv';
-    
+    let cantidadestudiantes = 'src/DATA/educacion/cantidadestudiantescs.csv';
 
     let scanner_trabajoestudio = parse({ delimiter: ',' }, function(err, data) {
       async.eachSeries(data, function(line, callback) {
@@ -34,7 +37,7 @@ sequelize.sync({ force: true }).then(async function() {
         array_apoyo_trabajo_escuela.push(line);
         contador_apoyo_trabajo_escuela = contador_apoyo_trabajo_escuela + 1;
         if (contador_apoyo_trabajo_escuela === 200) {
-           MunicipioController.aumentarApoyoTrabajoEscuela(
+          MunicipioController.aumentarApoyoTrabajoEscuela(
             array_apoyo_trabajo_escuela,
           );
         }
@@ -42,6 +45,24 @@ sequelize.sync({ force: true }).then(async function() {
       });
     });
 
+    let scanner_cantidadesestudiantes = parse({ delimiter: ',' }, function(
+      err,
+      data,
+    ) {
+      async.eachSeries(data, function(line, callback) {
+        //console.log(line, contador_apoyo_trabajo_escuela);
+        array_cantidad_estudiantes.push(line);
+        contador_cantidad_estudiantes = contador_cantidad_estudiantes + 1;
+        if (contador_cantidad_estudiantes === 24) {
+            MunicipioController.aumentarAlumnos(array_cantidad_estudiantes)
+        }
+        callback();
+      });
+    });
+
     fs.createReadStream(trabajoestudio).pipe(scanner_trabajoestudio);
+    fs.createReadStream(cantidadestudiantes).pipe(
+      scanner_cantidadesestudiantes,
+    );
   });
 });
--- BACKEND/src/controllers/municipioController.js
+++ BACKEND/src/controllers/municipioController.js
@@ -1,5 +1,6 @@
 const { municipio } = require('../models');
 let array_informacion = [0, 0, 0, 0];
+let array_informacion_alumnos = [0,0,0,0,0,0,0,0,0,0]
 
 function asyncFunction (item, cb) {
   setTimeout(() => {
@@ -21,6 +22,44 @@ function asyncFunction (item, cb) {
   }, 100);
 }
 
+function asyncFunction2 (item, cb) {
+  setTimeout(() => {
+    switch(item[2]){
+      case 'othon p. blanco':
+        array_informacion_alumnos[0] += parseInt(item[1])
+        break;
+      case 'jose ma. Morelos':
+        array_informacion_alumnos[1] += parseInt(item[1])
+        break;
+      case 'felipe carrillo puerto':
+        array_informacion_alumnos[2] += parseInt(item[1])
+        break;
+      case 'bacalar':
+        array_informacion_alumnos[3] += parseInt(item[1])
+        break;
+      case 'isla mujeres':
+        array_informacion_alumnos[4] += parseInt(item[1])
+        break;
+      case 'solidaridad':
+        array_informacion_alumnos[5] += parseInt(item[1])
+        break;
+      case 'benito juarez':
+        array_informacion_alumnos[6] += parseInt(item[1])
+        break;
+      case 'cozumel':
+        array_informacion_alumnos[7] += parseInt(item[1])
+        break;
+      case 'lazaro cardenas':
+        array_informacion_alumnos[8] += parseInt(item[1])
+        break;
+      case 'puerto morelos':
+        array_informacion_alumnos[9] += parseInt(item[1])
+        break;
+    }
+    cb();
+  }, 100);
+}
+
 module.exports = {
   aumentarApoyoTrabajoEscuela(arrayDatos) {
     let requests = arrayDatos.map((item) => {
@@ -28,7 +67,6 @@ module.exports = {
         asyncFunction(item, resolve);
       });
   })
-  
   Promise.all(requests).then(() => {
     municipio.update({
       apoyo_trabajo_escuela: array_informacion[0]
@@ -60,4 +98,83 @@ module.exports = {
     })
   });
   },
+  aumentarAlumnos(arrayDatos){
+    let requests = arrayDatos.map((item) => {
+      return new Promise((resolve) => {
+        asyncFunction2(item, resolve);
+      });
+  })
+  Promise.all(requests).then(() => {
+    municipio.update({
+      cantidad_estudiantes: array_informacion_alumnos[0] * 3
+    },{
+      where:{
+        nombre: 'Othon P. Blanco'
+      }
+    })
+    municipio.update({
+      cantidad_estudiantes: array_informacion_alumnos[1] * 3
+    },{
+      where:{
+        nombre: 'Jose Maria Morelos'
+      }
+    })
+    municipio.update({
+      cantidad_estudiantes: array_informacion_alumnos[2] * 3
+    },{
+      where:{
+        nombre: 'Felipe Carrillo Puerto'
+      }
+    })
+    municipio.update({
+      cantidad_estudiantes: array_informacion_alumnos[3] * 3
+    },{
+      where:{
+        nombre: 'Bacalar'
+      }
+    })
+    municipio.update({
+      cantidad_estudiantes: array_informacion_alumnos[4] * 3
+    },{
+      where:{
+        nombre: 'Isla Mujeres'
+      }
+    })
+    municipio.update({
+      cantidad_estudiantes: array_informacion_alumnos[5] * 3
+    },{
+      where:{
+        nombre: 'Solidaridad'
+      }
+    })
+    municipio.update({
+      cantidad_estudiantes: array_informacion_alumnos[6] * 3
+    },{
+      where:{
+        nombre: 'Benito Juarez'
+      }
+    })
+    municipio.update({
+      cantidad_estudiantes: array_informacion_alumnos[7] * 3
+    },{
+      where:{
+        nombre: 'Cozumel'
+      }
+    })
+    municipio.update({
+      cantidad_estudiantes: array_informacion_alumnos[8] * 3
+    },{
+      where:{
+        nombre: 'Lazaro Cardenas'
+      }
+    })
+    municipio.update({
+      cantidad_estudiantes: array_informacion_alumnos[9] * 3
+    },{
+      where:{
+        nombre: 'Puerto Morelos'
+      }
+    })
+  });
+}
 };
