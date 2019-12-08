84ac95b404870679c33b795be436b923f34943cf
luislortega <a18016323@alumnos.uady.mx>
2019-12-07 20:39:58 -0500

    Removiendo backend subido a cloud

--- BACKEND/src/config/routes.js
+++
@@ -1,7 +0,0 @@
-//Controladores
-const MunicipioController  = require('../controllers/municipioController.js');
-const EstadoController = require('../controllers/estadoController');
-module.exports = app => {
-  app.get('/obtenerDatosMunicipios', MunicipioController.obtenerDatos);
-  app.get('/obtenerDatosQuintanaRoo', EstadoController.obtenerDatos)
-};
