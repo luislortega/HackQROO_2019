7bda1a8547a6e8f72d2518b067e2c73c7b78d916
luislortega <leon.luisgerardo775@gmail.com>
2019-12-09 08:54:06 -0500

    Vistas de graficas dinamicas

--- App/vista_programa.php
+++ App/vista_programa.php
@@ -105,6 +105,8 @@ if (isset($_GET['id_programa'])) {
             });
             titulo = titulos[contador].innerHTML.replace('"'," ")
             titulo = titulo.replace('"', " ")
+            titulo = titulo.replace('[', " ")
+            titulo = titulo.replace('[', " ")
             console.log(array_labels)
             console.log(value_labels)
             //console.log(elementos)
@@ -134,6 +136,8 @@ if (isset($_GET['id_programa'])) {
     // Bar chart
 
     </script>
+    <h4> <a href="http://localhost/Php/Hackaton/HackQROO_2019/App/<?php echo $array_lista[0][3]; ?>">Descargar</a>  archivos CSV del programa.</h4>
+
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
